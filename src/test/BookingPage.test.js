c249f32950a5ccaba717b6edccae14f2701a4431
Viktor Korshunov <viktorkorshunov@Air-Viktor.home>
2023-12-23 09:01:53 +0100

    GitHub Pages

--- src/test/BookingPage.test.js
+++ src/test/BookingPage.test.js
@@ -7,13 +7,12 @@ describe ('InitializeTimes', () => {
 test('return initial array', () => {
     render(<BookingPage />);
     const expectedTimes = [
-        'Select',
         '17:00',
         '18:00',
         '19:00',
         '20:00',
         '21:00',
-        '22:00'
+        '22:00',
       ];
     expect(initializeTimes()).toEqual(expectedTimes);
 });
@@ -22,13 +21,12 @@ test('return initial array', () => {
 test('updateTimes return the same value from state', () => {
     render(<BookingPage />);
     const initialTimes = [
-        'Select',
         '17:00',
         '18:00',
         '19:00',
         '20:00',
         '21:00',
-        '22:00'
+        '22:00',
       ];
       const action = {type: 'update_times'};
     expect(updateTimes(initialTimes, action)).toEqual(initialTimes)
